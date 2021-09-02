d9ef279f1583f097393d86aa4c81cf18aff74723
emil41m5 <emil41m5@CV-BB-5340>
2021-09-02 10:32:27 +0200

    .

--- Opgave 1.3/Program.cs
+++ Opgave 1.3/Program.cs
@@ -6,11 +6,20 @@ namespace Opgave_1._3
     {
         private static void Calculate()
         {
+            Console.WriteLine("Skriv det første hele tal.");
+            int x = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Skriv det andet hele tal.");
+            int y = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("");
 
+            Console.WriteLine("Plus:     {0}", x + y);
+            Console.WriteLine("Minus:    {0}", x - y);
+            Console.WriteLine("Gange:    {0}", x * y);
+            Console.WriteLine("Division: {0}", x / y);
         }
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Calculate();
         }
     }
 }
---
+++ Opgave 1.4/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Opgave_1._4
+{
+    class Program
+    {
+        private static void isPositive
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
