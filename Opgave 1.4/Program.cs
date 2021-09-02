b379859edc4319a9bce029908f135ffb9d905b72
emil41m5 <emil41m5@CV-BB-5340>
2021-09-02 11:24:50 +0200

    .

--- Opgave 1.4/Program.cs
+++ Opgave 1.4/Program.cs
@@ -4,10 +4,21 @@ namespace Opgave_1._4
 {
     class Program
     {
-        private static void isPositive
+        private static bool isPositive(int x)
+        {
+            if (x < 0)
+                return false;
+            else
+                return true;
+        }
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Random rnd = new Random();
+            for (int i = 0; i < 100; i++)
+            {
+                int a = rnd.Next(int.MinValue, int.MaxValue);
+                Console.WriteLine(a + " " + isPositive(a));
+            }
         }
     }
 }
