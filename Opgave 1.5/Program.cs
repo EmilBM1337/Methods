d63c4e35327a05db5ed649837b74a62ff4d6dd71
emil41m5 <emil41m5@CV-BB-5340>
2021-09-06 11:35:43 +0200

    .

---
+++ Opgave 1.5/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Opgave_1._5
+{
+    class Program
+    {
+        private static void Calculate(int a)
+        {
+            Console.WriteLine("Kvadratet: " + a * a);
+            if (a >= 0)
+            Console.WriteLine("Kvadratroden: " + Math.Sqrt(a));
+
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Indtast et tal: ");
+            int x = int.Parse(Console.ReadLine());
+            Calculate(x);
+        }
+    }
+}
---
+++ Opgave 1.6/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Opgave_1._6
+{
+    class Program
+    {
+        static void IsEven(int number)
+        {
+
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Indtast et heltal: ");
+
+            IsEven();
+        }
+    }
+}
