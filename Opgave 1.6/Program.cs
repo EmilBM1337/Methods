45a0b26ea1d66449d2717535b4a21007ae2d324b
emil41m5 <emil41m5@CV-BB-5340>
2021-09-06 13:27:07 +0200

    .

--- Opgave 1.6/Program.cs
+++ Opgave 1.6/Program.cs
@@ -6,13 +6,20 @@ namespace Opgave_1._6
     {
         static void IsEven(int number)
         {
-
+            if (number % 2 == 0)
+            {
+                Console.WriteLine("Tallet er LIGE");
+            }
+            else
+            {
+                Console.WriteLine("Tallet er ULIGE");
+            }
         }
         static void Main(string[] args)
         {
             Console.WriteLine("Indtast et heltal: ");
-
-            IsEven();
+            int x = int.Parse(Console.ReadLine());
+            IsEven(x);
         }
     }
 }
