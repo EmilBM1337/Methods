61a74a946d1bae53c7845109dee3958223610611
emil41m5 <emil41m5@CV-BB-5340>
2021-09-07 10:56:23 +0200

    .

--- Opgave 1.7/Program.cs
+++ Opgave 1.7/Program.cs
@@ -4,9 +4,29 @@ namespace Opgave_1._7
 {
     class Program
     {
+        static bool HasEqualLenght(int[] numbers, string[] letters)
+        {
+            if (numbers.Length == letters.Length)
+                return true;
+            else
+                return false;
+        }
+        static void Print(int[] numbers)
+        {
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                Console.Write("Index " + i + " ");
+                Console.WriteLine(numbers[i]);
+            }
+        }
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int x = 2;
+            int y = 14;
+            int[] i = { x, y , 8, 16, 9};
+            string[] brothers = { "Groucho Marx", "Chico Marx", "Harpo Marx", "Zeppo Marx", "Gummo Marx" };
+            //Print(i);
+            Console.WriteLine(HasEqualLenght(i, brothers)); 
         }
     }
 }
---
+++ Opgave 1.9/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Opgave_1._9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
