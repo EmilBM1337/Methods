76ca54173f77cda4afc27a26906bca258e7decfa
emil41m5 <emil41m5@CV-BB-5340>
2021-09-08 14:44:47 +0200

    .

---
+++ Opgave 1.10/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Opgave_1._10
+{
+    class Program
+    {
+        public static double Median(int[] a, int n)
+        {
+            Array.Sort(a);
+
+            if (n % 2 != 0)
+                return (double)a[n / 2];
+
+            return (double)(a[(n - 1) / 2] + a[n / 2]) / 2.0;
+        }
+        public static void Main()
+        {
+            int[] a = { 1, 3, 4, 2, 7, 5, 8, 6 };
+            int n = a.Length;
+
+            Console.WriteLine("Medianen = " + Median(a, n) + "/n");
+        }
+    }
+}
--- Opgave 1.9/Program.cs
+++ Opgave 1.9/Program.cs
@@ -4,9 +4,24 @@ namespace Opgave_1._9
 {
     class Program
     {
-        static void Main(string[] args)
+        static float MiddelVærdien(float[]a, float n)
         {
+            float prb = (1 / n);
+            float sum = 0;
 
+            for (int i = 0; i < n; i++)
+                sum += a[i] * prb;
+
+            return sum;
+        }
+        public static void Main()
+        {
+            float expect, n = 6f;
+            float[] a = { 1f, 2f, 3f, 4f, 5f, 6f };
+
+            expect = MiddelVærdien(a, n);
+
+            Console.WriteLine("Middelværdien er: " + expect);
         }
     }
 }
